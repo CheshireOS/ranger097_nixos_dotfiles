a15788d62e0496d521074a1520dc3df95a1e93f3
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-15 18:05:03 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -72,7 +72,7 @@ eval "$(direnv hook bash)"
 };
 
 programs.bash.promptInit = ''
-PS1="\n\e[34m\]╭──\e[37m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\n╰─〉"
+PS1="\n\e[34m\]╭──\e[33m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\n\e[34m\]╰─\e[38m\]〉"
 
 '';
 
