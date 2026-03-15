f313fccf33814c2d466f4bac2760be0ca10525bd
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-15 14:13:16 -0400

    updated configs

--- packages.nix
+++ packages.nix
@@ -4,6 +4,7 @@ programs.steam.enable = true;
 programs.starship.enable = true;
 programs.direnv.enable = true;
 programs.direnv.silent = true;
+android_sdk.accept_license = true;
 environment.systemPackages = with pkgs; [
 sbctl
 ani-cli
