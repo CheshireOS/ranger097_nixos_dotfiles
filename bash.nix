6e055df6381cf86dbb7cedc5001cdb98c141119e
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-15 15:40:14 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -67,13 +67,11 @@ nix flake update
 
 
 export PS1="\[\e[1;36m\] \[\e[1;34m\]\w \[\e[1;32m\]\$(get_git) \n\[\e[1;37m\]   \[\e[0m\]"
-
 source "$HOME/.cache/wal/colors.sh"
 #(cat $HOME/.cache/wal/sequences)
 printf "\e]11;%s\a" "$color0"
 pokeget random --hide-name
 eval "$(direnv hook bash)"
-eval "$(starship init bash)"
 '';
 };
 }
