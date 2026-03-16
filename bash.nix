cda42f9f53393606144d56b8dabce1ac146d566c
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-15 21:50:12 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -17,12 +17,6 @@ cii = "cd ~/.config";
 ls = "lsd -a";
 ytdownload = "yt-dlp -x --audio-format mp3 --audio-quality 0 --cookies-from-browser firefox --embed-metadata --embed-thumbnail --add-metadata";
 };
-
-
-promptInit = ''
-PS1="\n\[\e[31m\]╭──\[\e[36m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\[\e[35m\]$(__git_ps1 \" (%s)\")\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
-'';
-
    
 interactiveShellInit = ''
 dgit() {
@@ -78,6 +72,10 @@ source "$HOME/.cache/wal/colors.sh"
 (cat $HOME/.cache/wal/sequences &)
 pokeget random --hide-name
 eval "$(direnv hook bash)"
+'';
+
+promptInit = ''
+PS1="\n\[\e[31m\]╭──\[\e[36m\]''$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)''\[\e[35m\]''$(__git_ps1 \" (%s)\")''\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
 '';
 };
 }
