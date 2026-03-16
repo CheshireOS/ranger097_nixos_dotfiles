22997a0ace6d4b3b6750047dbcfeb38b0cefae1a
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-15 21:35:30 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -68,12 +68,13 @@ source "$HOME/.cache/wal/colors.sh"
 (cat $HOME/.cache/wal/sequences &)
 pokeget random --hide-name
 eval "$(direnv hook bash)"
+PS1="\n\[\e[31m\]╭──\[\e[36m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\[\e[35m\]$(__git_ps1 \" (%s)\")\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
 '';
 };
 
-programs.bash.promptInit = ''
-PS1="\n\[\e[31m\]╭──\[\e[36m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\[\e[35m\]$(__git_ps1 \" (%s)\")\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
+#programs.bash.promptInit = ''
+#PS1="\n\[\e[31m\]╭──\[\e[36m\]$(case "$PWD" in "$HOME") echo "home" ;; "/") echo "root" ;; */ranger097_nixos_dotfiles) echo "dotfiles" ;; */.config) echo "configs" ;; * ) echo "\W" ;; esac)\[\e[35m\]$(__git_ps1 \" (%s)\")\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
 
-'';
+#'';
 
 }
