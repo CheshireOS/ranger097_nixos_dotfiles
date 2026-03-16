02ec06ccdab4ed75a3493912625eb5e493030f28
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-16 01:39:46 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -155,7 +155,7 @@ parse_git_branch() {
 '';
 
 promptInit = ''
-PS1="\n\[\e[31m\]╭──\[\e[36m\]\$(get_dir_name)\$(parse_git_branch)\[\e[32m\]\$(get_go_version)\[\e[35m\]\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
+PS1="\n\[\e[31m\]╭──\[\e[36m\]\$(get_dir_name)\$(parse_git_branch)\[\e[32m\]\$(get_lang_info)\[\e[35m\]\n\[\e[31m\]╰─\[\e[36m\]〉\[\e[0m\]"
 '';
 
 };
