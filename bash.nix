975664e7eed8da388d96d1608f95a2450a941b29
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-17 02:55:13 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -155,11 +155,11 @@ parse_git_branch() {
 
 song_playing=$(playerctl metadata --format "{{ artist }}" 2>/dev/null)
 function Music_fetch {
-fmt_song=$(printf "%-31.31s" "$song_playing")
+fmt_song=$(printf "%-20.20s" "$song_playing")
 if [ $? -eq 0 ]; then
-   echo -e "\e[31m│      \e[32m  \e[36m$fmt_song\e[31m│"
+   echo -e "\e[31m│  \e[32m  \e[36m$fmt_song\e[31m│"
 else
-   echo -e "\e[31m│      \e[32m  \e[36mNo Music Playing.$fmt_song\e[31m│"
+   echo -e "\e[31m│  \e[32m  \e[36mNo Music Playing.$fmt_song\e[31m│"
 fi
 }
 
