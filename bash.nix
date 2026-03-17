4f6817a3ac6360e5d711e3d624a57f3675a956bb
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-17 00:39:09 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -204,8 +204,8 @@ fi
 }
 
 ranger_fetch() {
-local img_path="/home/ranger/Downloads/4ef7748aae1eee9f9140c5ebf1649bcb.gif"
-timg -g 35x35 "$img_path" &
+local img_path="/home/ranger/Downloads/JJK_1.jpeg"
+timg -g 35x35 "$img_path"
 echo -e "\e[31m╭───────────────────────────────────────╮"
 Music_fetch 
 Host_fetch 
