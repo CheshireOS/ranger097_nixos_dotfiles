2ff15d9bdb9194d973be8d3a21e850b56475df12
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-17 02:47:38 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -211,9 +211,9 @@ function ascii_art {
 #fmt_mid=$(printf "%-35.35s" "$mid_ascii")
 #fmt_bot=$(printf "%-35.35s" "$bot_ascii")
 echo -e "\e[31m│                                       \e[31m│"
-echo -e "\e[31m│        \e[36m$top_ascii      \e[31m│"
-echo -e "\e[31m│        \e[36m$mid_ascii      \e[31m│"
-echo -e "\e[31m│        \e[36m$bot_ascii      \e[31m│"
+echo -e "\e[31m│       \e[32m$top_ascii       \e[31m│"
+echo -e "\e[31m│       \e[32m$mid_ascii       \e[31m│"
+echo -e "\e[31m│       \e[32m$bot_ascii       \e[31m│"
 echo -e "\e[31m│                                       \e[31m│"
 }
 
