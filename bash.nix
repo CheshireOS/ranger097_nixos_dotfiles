8d9832dac145a930de39375720f274e4a2e2fbff
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-18 01:39:54 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -218,7 +218,6 @@ echo -e "\e[31m│                                 \e[31m│"
 
 ranger_fetch() {
 echo -e "\n"
-echo -e "\n"
 term_width=$(tput cols)
 top_line="╭─────────────────────────────────╮"
 top_line_len=$(echo -ne "$top_line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
