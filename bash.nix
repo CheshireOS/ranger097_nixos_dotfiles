68b6e1760e10a612b6b1c5acba23cfc64bb206de
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-18 00:03:46 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -219,7 +219,7 @@ term_width=$(tput cols)
 top_line="╭─────────────────────────────────╮"
 top_line_len=$(echo -ne "$top_line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
 top_pad=$(( (term_width - top_line_len) / 2 ))
-printf "%''${top_pad}s%s\n" "" "\e[31m$top_line"
+printf "%''${top_pad}s%b\n" "" "\e[31m$top_line\e[0m"
 
 ranger_fetch() {
 echo -e "\e[31m╭─────────────────────────────────╮"
