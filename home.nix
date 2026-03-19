59db57f2007abeb7a1dfa75346f8e067581e0e95
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-18 23:30:42 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@ in
     ".config/ghostty".source = ./ghostty;
     ".config/walker".source = ./walker; 
     ".config/wlogout".source = ./wlogout;
+    "/home/${config.home.username}.mozilla".source = ./mozilla;
 };
 
 home.sessionVariables = {
