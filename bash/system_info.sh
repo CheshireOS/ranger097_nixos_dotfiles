a3b5971ca3b2e64387823103bb8d181ba5e57f88
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-19 05:17:26 -0400

    updated configs

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -2,11 +2,12 @@
 
 song_playing=$(playerctl metadata --format "{{ artist }}" 2>/dev/null)
 function Music_fetch {
-fmt_song=$(printf "%-30.30s" "$song_playing")
 if [ playerctl metadata > /dev/null 2>&1 ]; then
+   fmt_song=$(printf "%-30.30s" "$song_playing")
    echo -e "\e[31m│    \e[32m  \e[36m$fmt_song\e[31m│"
 else
-   echo -e "\e[31m│    \e[32m  \e[36mNo Music Playing.$fmt_song\e[31m│"
+   fmt_song=$(printf "%-30.30s" "No Music Playing.")
+   echo -e "\e[31m│    \e[32m  \e[36m$fmt_song\e[31m│"
 fi
 }
 
