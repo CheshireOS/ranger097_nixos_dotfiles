b6d6f0478cdb5afea8b044fd711015e4c694d701
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-18 23:19:51 -0400

    updated configs

--- python/waybar_switcher/waybar_switcher.py
+++ python/waybar_switcher/waybar_switcher.py
@@ -9,14 +9,14 @@ with open(toggle_file, 'r') as f:
     
 try:
     if x == 0:
-        subprocess.run(["waybar", "-c", "/home/ranger/ranger097_nixos_dotfiles/waybar/top.jsonc", "-s", "/home/ranger/ranger097_nixos_dotfiles/waybar/top.css"])
-        subprocess.run(["waybar", "-c", "/home/ranger/ranger097_nixos_dotfiles/waybar/bottom.jsonc", "-s", "/home/ranger/ranger097_nixos_dotfiles/waybar/bottom.css"])
+        subprocess.Popen(["waybar", "-c", "/home/ranger/ranger097_nixos_dotfiles/waybar/top.jsonc", "-s", "/home/ranger/ranger097_nixos_dotfiles/waybar/top.css"])
+        subprocess.Popen(["waybar", "-c", "/home/ranger/ranger097_nixos_dotfiles/waybar/bottom.jsonc", "-s", "/home/ranger/ranger097_nixos_dotfiles/waybar/bottom.css"])
         with open(toggle_file, 'w') as f:
             f.write("1")
     else:
         subprocess.run(["pkill", "waybar"])
         with open(toggle_file, 'w') as f:
             f.write("0")
-            
+
 except KeyboardInterrupt:
     sys.exit(0)
