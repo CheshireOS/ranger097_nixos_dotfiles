c32a2b72b05a4cda0bce379b635232697f62dac5
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-19 04:32:53 -0400

    updated configs

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -55,9 +55,9 @@ mid_ascii="┃┗┫   ┃   ┏╋┛   ┃ ┃   ┗━┓"
 bot_ascii="╹ ╹   ╹   ╹ ╹   ┗━┛   ┗━┛"
 function ascii_art {
 echo -e "\e[31m│                                 \e[31m│"
-echo -e "\e[31m│    \e[32m$top_ascii    \e[31m│"
-echo -e "\e[31m│    \e[32m$mid_ascii    \e[31m│"
-echo -e "\e[31m│    \e[32m$bot_ascii    \e[31m│"
+echo -e "\e[31m│      \e[32m$top_ascii      \e[31m│"
+echo -e "\e[31m│      \e[32m$mid_ascii      \e[31m│"
+echo -e "\e[31m│      \e[32m$bot_ascii      \e[31m│"
 echo -e "\e[31m│                                 \e[31m│"
 }
 
@@ -137,7 +137,7 @@ color_line_len=$(echo -ne "$top_line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
 top_pad=$(( (term_width - color_line_len) / 2 ))
 printf "%''${top_pad}s%b\n" "" "\e[31m$color_line\e[0m"
 
-space_line="\e[31m│                                 \e[31m│"
+space_line="\e[31m│                                     \e[31m│"
 space_line_len=$(echo -ne "$space_line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
 top_pad=$(( (term_width - space_line_len) / 2 ))
 printf "%''${top_pad}s%b\n" "" "\e[31m$space_line\e[0m"
