2fa4e433333c813fb148d948391649af3686b1a2
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-19 05:04:01 -0400

    updated configs

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -102,6 +102,8 @@ line_pad=$(( (term_width - line_len) / 2 ))
 printf "%''${line_pad}s%b\n" "" "$line"
 done
 
+echo -e "\e[31m│                                     \e[31m│"
+
 Music_fetch | while read -r line; do
 line_len=$(echo -ne "$line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
 line_pad=$(( (term_width - line_len) / 2 ))
