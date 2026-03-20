06445527df3b12422a08cd31e8569a7f295bb05d
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-20 02:22:18 -0400

    updated configs

---
+++ shell.nix
@@ -0,0 +1,65 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+packages = with pkgs; [
+go
+gopls
+delve
+
+(python3.withPackages (pyPkgs: with pyPkgs; [
+python-lsp-server
+npyscreen
+black
+]))
+
+gcc
+gnumake
+cmake
+gdb
+jdk21
+maven
+gradle
+nodejs_22
+elixir
+elixir-ls
+rustc
+cargo
+rust-analyzer
+rustfmt
+docker
+docker-compose
+podman
+ripgrep
+fd
+fzf
+jq
+chsharpier
+erb_format
+fantomas
+fish_indent
+markdown-toc
+markdownlint-cli2
+nixfmt
+prettier
+rubocop
+sqlfluff
+ast-grep
+luarocks
+];
+
+
+shellHook = ''
+echo "rangers dev shell"
+
+
+'';
+
+
+
+} 
+
+
+
+
+
+
