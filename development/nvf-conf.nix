04fd5e284ed5cdfba3ae99fad4559e25124a9fdb
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-20 20:49:52 -0400

    updated configs

--- development/nvf-conf.nix
+++ development/nvf-conf.nix
@@ -18,7 +18,26 @@ programs.nvf = {
          enable = true;
          transparent = true;
 	 name = "base16";
-};
+         base16-colors = {
+      	 base00 = "000000"; #walColors.special.background; 
+         base01 = "acacac"; #walColors.colors.color0;      
+         base02 = "ff0000"; #walColors.colors.color8;      
+         base03 = "ff0000";     
+         base04 = "ff0000"; #walColors.colors.color7;      
+         base05 = "ff0000"; #walColors.special.foreground; 
+         base06 = "ff0000"; #walColors.colors.color7;      
+         base07 = "ff0000"; #walColors.colors.color15;     
+         base08 = "ff0000"; #walColors.colors.color1;     
+         base09 = "ff0000"; #walColors.colors.color9;      
+         base0A = "ff0000"; #walColors.colors.color3;      
+         base0B = "ff0000"; #walColors.colors.color2;     
+         base0C = "ff0000"; #walColors.colors.color6;      
+         base0D = "ff0000"; #walColors.colors.color4;      
+         base0E = "ff0000"; #walColors.colors.color5;      
+         base0F = "ff0000"; #walColors.colors.color13;     
+    };
+
+         };
        
        extraPlugins = with pkgs.vimPlugins; {
           wal-vim = {
