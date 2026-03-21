29417ef524e760ab8bb21410e4fbd9a80f3eb369
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-21 02:13:54 -0400

    updated configs

--- development/nixvim.nix
+++ development/nixvim.nix
@@ -3,16 +3,109 @@
     programs.nixvim = {
     enable = true;
     defaultEditor = true;
-
-    colorschemes.gruvbox.enable = true;
-    plugins.lualine.enable = true;
+    colorschemes.gruvbox.enable = false;
     
+    plugins.treesitter = {
+    enable = true;
+    settings.highlight.enable = true;
+    settings.indent.enable = true;
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+    javascript
+    typescript
+    tsx
+    svelte
+    html
+    css
+    go
+    python
+    rust
+    java
+    gdscript
+    nix
+    ];
+    };
+    
+    plugins.lualine.enable = true;
+    plugins.luasnip.enable = true;
+    plugins.rustaceanvim.enable = true;
+    plugins.lsp.enable = true;
+    plugins.java.enable = true;
+    plugins.auto-save.enable = true;
+    plugins.dotnet.enable = true;
+    plugins.fzf-lua.enable = true;
+    plugins.gdscript-extended-lsp.enable = true;
+    plugins.git-conflict.enable = true;
+    plugins.git-worktree.enable = true;
+    plugins.git-worktree.enableTelescope = true;
+    plugins.godot.enable = true;
+    plugins.harpoon.enable = true;
+    plugins.harpoon.enableTelescope = true;
+    plugins.harpoon.autoLoad = true;
+    plugins.cmp = {
+      enable = true;
+      settings.sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+    };   
     opts = {
       number = true;         
       relativenumber = true; 
       shiftwidth = 2;   
+      termguicolors = true;
     };
+    
+    highlight = {
+      Normal.bg = "none";
+      NonText.bg = "none";
+      NormalFloat.bg = "none";
+      SignColumn.bg = "none";
+      StatusLine.bg = "none";
+      VertSplit.bg = "none";
+    };
+
+      plugins.lsp.servers = {
+      pyright.enable = true;
+      rust_analyzer = {
+        enable = true;
+        installCargo = true;
+        installRustc = true;
+      };
+      nixd.enable = true;
+      clangd.enable = true;
+      ts_ls.enable = true;
+      html.enable = true;
+      cssls.enable = true;
+      gopls.enable = true;
+      lua_ls.enable = true;
+      svelte.enable = true;
+      tailwindcss.enable = true; 
+      emmet_ls.enable = true;
+      jdtls.enable = true; 
+      gdscript.enable = true;
+};
+
+    plugins.transparent = {
+      enable = true;
+      settings.extra_groups = [
+        "NormalFloat"
+        "NvimTreeNormal"
+      ];
+    };
+   
+    plugins.none-ls = {
+    enable = true;
+    sources.formatting = {
+    prettier.enable = true; # Handles HTML, CSS, JS, React, Svelte
+    gofmt.enable = true;    # Handles Go
+    black.enable = true;    # Handles Python
+      };
+    };
+    
+
   };
+
 
 
 
