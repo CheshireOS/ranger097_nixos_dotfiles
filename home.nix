05d131f55d7f5c424aa8c22cabf391b3e2158b26
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-21 03:59:58 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -36,40 +36,6 @@ home.sessionVariables = {
 };
 
 
-programs.neovim = {
-enable = true;
-plugins = with pkgs.vimPlugins; [
-direnv-vim
-wal-vim
-nvim-lspconfig
-nvim-treesitter
-rustaceanvim
-nvim-jdtls
-go-nvim
-vim-rails
-tailwind-tools-nvim
-nvim-cmp
-cmp-nvim-lsp
-luasnip
-fidget-nvim
-conform-nvim
-nvim-lint
-nvim-dap
-nvim-dap-ui
-nvim-dap-python
-lazygit-nvim
-];
-
-
-
-};
-
-
-
-
-
-
-
 
 
 wayland.windowManager.hyprland = {
