273bbc0ce4c1c76bc15ad6b84502ad018889f4ab
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-21 03:23:19 -0400

    updated configs

--- development/nixvim.nix
+++ development/nixvim.nix
@@ -1,4 +1,10 @@
 { config, pkgs, lib, ... }:{
+  
+    let 
+       pywal = pkgs.vimPlugins.pywal-nvim;
+
+
+    in
  
     programs.nixvim = {
     enable = true;
@@ -130,19 +136,12 @@
     };
     
     extraConfigVim = ''
-      local function set_transparency()
-        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
-      end
-
-      set_transparency()
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = set_transparency,
-      })
     '';
-    
+
+    extraPlugins = [
+    pywal
+    ];
+        
     extraPackages = with pkgs; [
     tree-sitter
     nodePackages.prettier
