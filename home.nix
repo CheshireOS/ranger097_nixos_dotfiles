beb2f05fe8f9d0ccbee511aaa1e16f398e2fb132
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-21 04:31:55 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -289,7 +289,7 @@ bindl = , XF86AudioPrev, exec, playerctl previous
 #KEYBINDING_SETTINGS_END
 
 #WINDOW_RULE_SETTINGS_START
-windowrule = match:class com.mitchellh.ghostty, opacity 0.7
+windowrule = match:class com.mitchellh.ghostty, opacity 1.0
 windowrule = match:class code, opacity 0.7
 windowrule = match:class firefox, opacity 1.0
 windowrule = match:class discord, opacity 1.0
