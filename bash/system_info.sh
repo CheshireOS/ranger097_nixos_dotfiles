471b06d3b06cdb3572a6405b160fa3c3c4773496
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-23 04:13:00 -0400

    updated configs

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -2,14 +2,17 @@
 
 width=$(tput cols)
 math=$(( width /  2 ))
-percent=$( echo "math * 0.05" | bc -l )
-padding=$( echo "math - percent" | bc -l )
-padding=$(printf "%.0f" "$padding")
+percent=$(( width / 7 ))
+padding=$(( math - percent ))
+
+function pokemon() {
+poke=$(pokeget --mega random --hide-name)
+printf "%*s" "$padding" "$poke"
+} 
 
 function Music_fetch {
 local song_playing=$(playerctl metadata --format "{{ artist }}" 2>/dev/null)
 local status=$(playerctl status 2>/dev/null)
-
 if [ "$song_playing" != "No players found" ] && [ -n "$song_playing" ]; then
     printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "$song_playing"
 else
@@ -17,78 +20,67 @@ else
 fi
 }
 
-
 function Host_fetch {
-host_name=$(hostnamectl | awk 'NR==1 {print $3}')
-fmt_host=$(printf "%-32.32s" "Host $host_name")
+local host_name=$(hostnamectl | awk 'NR==1 {print $3}')
 if [ $? -eq 0 ]; then
-  printf "%*s\e[32m󰐝  \e[36m%s\e[0m\n" "$padding" "" "$fmt_host" 
+     printf "%*s\e[32m󰐝  \e[36m%s\e[0m\n" "$padding" "" "$host_name" 
 else
-  printf "%*s\e[32m󰐝  \e[36m%s\e[0m\n" "$padding" "" "WTF???" 
+     printf "%*s\e[32m󰐝  \e[36m%s\e[0m\n" "$padding" "" "wtf???" 
 fi
 }
 
-Operating_system=$(hostnamectl | awk 'NR==6 {print $3, $4, $5 }')
 function OS_fetch {
-fmt_os=$(printf "%-32.32s" "$Operating_system")
+local operating_system=$(hostnamectl | awk 'NR==6 {print $3, $4, $5 }')
 if [ $? -eq 0 ]; then
-   echo -e "    \e[32m  \e[36m$fmt_os"
+    printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "$operating_system" 
 else
-   echo -e "    \e[32m  \e[36mWTF$fmt_os"
+    printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "wtf???" 
 fi
 }
 
-Kernel_version=$(hostnamectl | awk 'NR==8 {print $2, $3}')
 function kernel_fetch {
-fmt_kernel=$(printf "%-32.32s" "$Kernel_version")
+local kernel_version=$(hostnamectl | awk 'NR==8 {print $2, $3}')
 if [ $? -eq 0 ]; then
-   echo -e "    \e[32m  \e[36m$fmt_kernel"
+   printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "$kernel_version" 
 else
-   echo -e "    \e[32m  \e[36mBro really$fmt_kernel"
+   printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "wtf???"
 fi
 }
 
-IP_address=$(curl icanhazip.com 2> /dev/null )
 function IP_fetch {
-fmt_ip=$(printf "%-32.32s" "$IP_address")
+ip_address=$(curl icanhazip.com 2> /dev/null )
 if [ $? -eq 0 ]; then
-   echo -e "    \e[32m󰖩  \e[36m$fmt_ip"
+   printf "%*s\e[32m󰖩  \e[36m%s\e[0m\n" "$padding" "" "$ip_address" 
 else
-   echo -e "    \e[32m󰖩  \e[36mNO IP ADDRESS$fmt_ip"
+   printf "%*s\e[32m󰖩  \e[36m%s\e[0m\n" "$padding" "" "???" 
 fi
 }
 
-top_ascii="┏┓╻   ╻   ╻ ╻   ┏━┓   ┏━┓"
-mid_ascii="┃┗┫   ┃   ┏╋┛   ┃ ┃   ┗━┓"
-bot_ascii="╹ ╹   ╹   ╹ ╹   ┗━┛   ┗━┛"
 function ascii_art {
-echo -e "                                       "
-echo -e "       \e[32m$top_ascii       "
-echo -e "       \e[32m$mid_ascii       "
-echo -e "       \e[32m$bot_ascii       "
-echo -e "                                       "
+local top_ascii="┏┓╻   ╻   ╻ ╻   ┏━┓   ┏━┓"
+local mid_ascii="┃┗┫   ┃   ┏╋┛   ┃ ┃   ┗━┓"
+local bot_ascii="╹ ╹   ╹   ╹ ╹   ┗━┛   ┗━┛"
+echo -e "\n"
+printf "%*s\e[32m%s\e[0m\n" "$padding" "" "$top_ascii" 
+printf "%*s\e[32m%s\e[0m\n" "$padding" "" "$mid_ascii" 
+printf "%*s\e[32m%s\e[0m\n" "$padding" "" "$bot_ascii" 
 }
 
 function github_info {
-repo_name="ranger097"
-update_time=$(gh repo view ranger097_nixos_dotfiles --json updatedAt --template '{{.updatedAt | timeago}}')
-forks_count=$(gh repo view ranger097_nixos_dotfiles --json forkCount --jq '.forkCount')
-stars_count=$(gh repo view ranger097_nixos_dotfiles --json stargazerCount --jq '.stargazerCount')
-fmt_name=$(printf "%32s" "\e[32m \e[36m$repo_name")
-fmt_update=$(printf "%32s" "\e[32m \e[36m$Last push $update_time")
-fmt_forks=$(printf "%32s" "\e[32m  \e[36m$Forks $forks_count")
-fmt_stars=$(printf "%32s" "\e[32m \e[36m$Stars $stars_count")
-echo -e "$fmt_name"
-echo -e "$fmt_stars"
-echo -e "$fmt_forks"
-echo -e "$fmt_update"
-echo -e "                                       "
+local repo_name="ranger097"
+local update_time=$(gh repo view ranger097_nixos_dotfiles --json updatedAt --template '{{.updatedAt | timeago}}')
+local forks_count=$(gh repo view ranger097_nixos_dotfiles --json forkCount --jq '.forkCount')
+local stars_count=$(gh repo view ranger097_nixos_dotfiles --json stargazerCount --jq '.stargazerCount')
+printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "$repo_name" 
+printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "Stars $stars_count" 
+printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "Forks $forks_count" 
+printf "%*s\e[32m  \e[36m%s\e[0m\n" "$padding" "" "Last update was $update_time"
 }
 
-
-color_line="\e[32m  \e[31m󰟪 \e[32m󰟪 \e[33m󰟪 \e[34m󰟪 \e[35m󰟪 \e[36m󰟪 \e[37m󰟪 "
-color_line_len=$(echo -ne "$top_line" | sed 's/\x1b\[[0-9;]*m//g' | wc -m)
-printf "%*s%s\n" ${top_pad} "" "$color_line\e[0m"
+colors() {
+local color_line=$'\e[32m󰮯  \e[31m󰊠 \e[32m󰊠 \e[33m󰊠 \e[34m󰊠 \e[35m󰊠 \e[36m󰊠 \e[37m󰊠 '
+printf "%*s%s\e[0m\n" "$padding" "" "$color_line" 
+}
 
 ranger_fetch() {
 echo -e "\n"
@@ -99,11 +91,7 @@ Host_fetch
 kernel_fetch
 OS_fetch
 IP_fetch
-
-
-
-
-
+colors
 }
 
 printf '\033[3 q'
@@ -115,7 +103,7 @@ function ranger_page() {
     echo -e "\e]11;#000000\a"
     read -s -n 1 -p ""
     clear
-    pokeget random --hide-name
+    pokemon
     tput cnorm
 }
 
@@ -123,7 +111,7 @@ if [[ $(tput lines) -ge 25 && $(tput cols) -ge 60 ]]; then
    ranger_page
 
 else [[ $(tput lines) -ge 12 && $(tput cols) -ge 48 ]];
-   pokeget --mega random --hide-name
+   pokemon
     echo -e "\e]11;#000000\a"
    read -s -n 1 -p ""
    clear
