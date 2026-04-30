86d9b34a2cc3cb5781270863f4a41d3476395965
dcsc <sdc>
2026-04-30 16:19:55 -0400

    test

---
+++ bash/github.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+source /persist/home/ranger/ranger097_nixos_dotfiles/bash/ssh.sh
+make_ssh
+cd /persist/home/ranger/ranger097_nixos_dotfiles
+git add .
+read -p "changes: " changes
+git commit -m "$changes"
+git push origin main 
