dfa66b882a7532a1552679e3750ffad39263c9c0
dsc <dcs>
2026-04-30 16:48:32 -0400

    tweaked github script

--- bash/github.sh
+++ bash/github.sh
@@ -4,7 +4,7 @@ cd /persist/home/ranger/ranger097_nixos_dotfiles
 git add .
 echo "Lets save and push to github"
 read -p $'\e[33m  \e[32mchanges  \e[37m| ' changes
-git commit -m "$changes"
+git commit -m "$changes" > /dev/null 2>&1
 git push origin main > /dev/null 2>&1
 
 
