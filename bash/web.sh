601683231c098df7f085fa1959bdda3394a793f1
ranger097 <ranger097@proton.me>
2026-05-04 18:23:44 -0400

    made theme system for website, added new themes, made layout more readable and flexable.

--- bash/web.sh
+++ bash/web.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env
 web() {
 cd /persist/home/ranger/ranger097_nixos_dotfiles/docs/
-xdg-open index.html
+nohup xdg-open index.html &
 nvim /persist/home/ranger/ranger097_nixos_dotfiles/docs/
-
 }
 
 web
--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,37 @@
-const navSection = document.getElementById('nav-section');
-const closeBtn = document.getElementById('close-btn');
-closeBtn.addEventListener('click',() => {
-  navSection.classList.toggle('hidden');
+const themeButtons = document.querySelectorAll('#anonymous, #gruvbox, #catppuccin, #kanagawa, #persona, #tokyo, #anime');
+
+const navList = document.querySelectorAll('.query_nav-text');
+navList.forEach((nav) => {
+  nav.addEventListener('click', function() {
+    const currentChosen = document.querySelector('.query_nav-text.chosen'); 
+    if (currentChosen) {
+      currentChosen.classList.remove('chosen');
+    }
+    this.classList.add('chosen');
+  });
 });
+
+const themeList = document.querySelectorAll('.theme-text');
+   themeList.forEach((theme) => {
+   theme.addEventListener('click', function() {
+   const currentTheme = document.querySelector('.theme-text.chosen');
+   if (currentTheme) {
+      currentTheme.classList.remove('chosen');
+   }
+      this.classList.add('chosen');
+   });
+});
+
+function themeChanger(newTheme) {
+thebody.className = newTheme;
+}
+
+themeButtons.forEach(btn => {
+  btn.addEventListener('click', () =>  themeChanger(btn.id));
+});
+
+
+thebody.classList.add('anonymous');
+document.getElementById('welcome')?.classList.add('chosen');
+document.getElementById('anonymous')?.classList.add('chosen');
+
