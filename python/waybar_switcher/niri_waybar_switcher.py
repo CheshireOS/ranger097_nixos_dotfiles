62b123683a15f96061e3cf35dbe44fa48c209a62
ranger097 <ranger097@proton.me>
2026-08-13 17:30:19 -0400

    added and configured waybar for niri

---
+++ python/waybar_switcher/niri_waybar_switcher.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import subprocess
+
+toggle_file = "/home/ranger/ranger097_nixos_dotfiles/python/waybar_switcher/toggle.txt"
+
+with open(toggle_file, 'r') as f:
+    x = int(f.readline().strip())
+    
+try:
+
+    if x == 0:
+        subprocess.run(["pkill", "waybar"])
+        subprocess.Popen(["waybar", "-c", 
+        "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_bottom.jsonc", "-s", 
+        "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_bottom.css"
+        ])
+        with open(toggle_file, 'w') as f:
+            f.write("1")
+
+    elif x == 1:
+        subprocess.run(["pkill", "waybar"])
+        subprocess.Popen(["waybar", "-c", 
+        "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_top.jsonc", "-s", 
+        "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_top.css"
+        ])
+        with open(toggle_file, 'w') as f:
+            f.write("2")
+
+    elif x == 2:
+      subprocess.run(["pkill", "waybar"])
+      subprocess.Popen(["waybar", "-c", 
+      "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_bottom.jsonc", "-s", 
+        "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_bottom.css"
+      ])
+      subprocess.Popen(["waybar", "-c", 
+      "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_top.jsonc", "-s", 
+      "/home/ranger/ranger097_nixos_dotfiles/waybar/niri_top.css"
+      ])
+
+      with open(toggle_file, 'w') as f:
+           f.write("3")
+    
+
+    else:
+       subprocess.run(["pkill", "waybar"])
+       with open(toggle_file, 'w') as f:
+            f.write("0")
+
+except KeyboardInterrupt:
+    sys.exit(0)
