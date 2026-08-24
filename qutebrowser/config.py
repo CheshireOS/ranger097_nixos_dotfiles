6e1423db92c4735004b9e6adc01ae120f2712a0f
ranger097 <ranger097@proton.me>
2026-08-23 22:01:19 -0400

    edited transparency

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,8 +5,7 @@ c.statusbar.show = "in-mode"
 c.tabs.close_mouse_button = "right"  
 c.url.start_pages = [
 "https://www.wgu.edu",
-"https://www.youtube.com",
-"https://www.nixos.org"
+"https://www.youtube.com"
 ]
 c.tabs.title.alignment = "center"
 c.tabs.tabs_are_windows = False
@@ -17,11 +16,13 @@ c.content.javascript.enabled = True
 c.content.geolocation = False
 c.content.fullscreen.window = True
 c.window.transparent = True
-c.colors.tabs.bar.bg = "#000000"
-c.colors.tabs.even.bg = "#000000"
-c.colors.tabs.odd.bg = "#000000"
-c.colors.statusbar.normal.bg = "#000000"
-c.colors.webpage.bg = "#000000"
+c.colors.tabs.bar.bg = "#101010"
+c.colors.tabs.even.bg = "#101010"
+c.colors.tabs.odd.bg = "#101010"
+c.colors.statusbar.normal.bg = "#101010"
+c.colors.webpage.bg = "#101010"
+#c.colors.webpage.darkmode.algorithm = "brightness-rgb"
+#c.colors.webpage.darkmode.contrast = 1.0
 #c.content.user_stylesheets = []
 
 
