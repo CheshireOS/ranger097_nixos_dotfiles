25b8ec986576be85a26333a9e6f7e361b56fb89f
ranger097 <ranger097@proton.me>
2026-08-25 15:45:39 -0400

    added pywal

--- home.nix
+++ home.nix
@@ -40,9 +40,10 @@ in
     ".config/niri/config.kdl".source = ./niri/config.kdl;
     ".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
     ".config/qutebrowser/config.py".source = ./qutebrowser/config.py;
-    ".config/wal".source = ./wal;
+    ".config/wal/colorschemes/dark".source = ./wal/colorschemes/dark;
+    ".config/wal/colorschemes/light".source = ./wal/colorschemes/light;
+    ".config/wal/templates".source = ./wal/templates;
   };
-
 
 programs.freetube = {
    enable = true;
